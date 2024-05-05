8978d42b90a3c7efc26f042a41f8d2452485e9f4
Erick Rick <erickcredjes@gmail.com>
2024-05-05 17:29:24 -0300

    Implementando processo para o IdentityUser e configuração DI

--- Program.cs
+++ Program.cs
@@ -1,11 +1,34 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using UsuariosApi.Data;
+using UsuariosApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<UsuarioDbContext>(options =>
+{
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Configuração do Identity 
+builder.Services
+  .AddIdentity<Usuario, IdentityRole>()
+  .AddEntityFrameworkStores<UsuarioDbContext>()
+  .AddDefaultTokenProviders();
+
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{ // Configuração para documentação do Swagger
+  config.SwaggerDoc("v1", new OpenApiInfo { Title = "IdentityUser", Version = "v1" });
+  var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+  var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+  config.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 

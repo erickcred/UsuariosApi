e5f578d29517e84a3fe3cf27eaf020dde4859365
Erick Rick <erickcredjes@gmail.com>
2024-05-09 22:40:55 -0300

    Incluindo Authentication e Policy para validação do acesso quando passar o Token

---
+++ Authorization/IdadeAuthorization.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace UsuariosApi.Authorization;
+
+public class IdadeAuthorization : AuthorizationHandler<IdadeMinima>
+{
+  protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IdadeMinima requirement)
+  {
+    var dataNascimentoClaim = context.User.FindFirst(claim => claim.Type == ClaimTypes.DateOfBirth);
+
+    if (dataNascimentoClaim is null)
+      return Task.CompletedTask;
+
+    var dataNascimento = Convert.ToDateTime(dataNascimentoClaim.Value);
+
+    var idadeDoUsuario = DateTime.Today.Year - dataNascimento.Year;
+
+    if (dataNascimento > DateTime.Today.AddYears(-idadeDoUsuario))
+      idadeDoUsuario--;
+
+    if (idadeDoUsuario > requirement.Idade)
+      context.Succeed(requirement);
+
+    return Task.CompletedTask;
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using UsuariosApi.Authorization;
 using UsuariosApi.Data;
 using UsuariosApi.Models;
 using UsuariosApi.Services;
@@ -21,6 +26,30 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<TokenService>();
 
+builder.Services.AddAuthentication(options =>
+{
+  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+  options.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuerSigningKey = true, // validação da chave gerada para o Token
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("@#$@#fdsasd{}__+211_*&¨sfddfsdfsdfsdds<>Ddsdfdsdf")),
+    ValidateAudience = false,
+    ValidateIssuer = false,
+    ClockSkew = TimeSpan.Zero // ele faz o get ou set do relógio para validação de tempo
+  };
+});
+builder.Services.AddSingleton<IAuthorizationHandler, IdadeAuthorization>();
+
+builder.Services.AddAuthorization(options =>
+{
+  options.AddPolicy("IdadeMinima", policy =>
+  {
+    policy.AddRequirements(new IdadeMinima(18));
+  });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -37,6 +66,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
